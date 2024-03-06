dbe4573ff54dadd501b52e58fc7804acd9b6344e
shaik mazhar ali <Shkmzhr@gmail.com>
2024-03-06 22:54:25 +0530

    second solution

--- myApp.js
+++ myApp.js
@@ -1,5 +1,14 @@
-let express = require('express');
-let app = express();
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello there!!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 
 
